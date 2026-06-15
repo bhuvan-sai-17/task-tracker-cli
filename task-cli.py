3009b01c81e3311d150117a712742c02beffea19
bhu1Sai <kasukurthibhuvan@gmail.com>
2026-06-16 01:43:52 +0200

    feat: create tasks.json file if it does not exist

--- task-cli.py
+++ task-cli.py
@@ -1,8 +1,53 @@
 import sys
+import os
+import pathlib
+import json
 
 
-command = sys.argv[1]
-task = sys.argv[2:]
+jf = "\\tasks.json"
+json_file_path = str(pathlib.Path().absolute()) + jf
 
-print(f"command: {command}")
-print(f"task: {task}")
+
+# Returns all the tasks saved in the json file
+def load_tasks():
+
+    # create the json file if it dose not exist
+    if os.path.exists(json_file_path):
+        print("The json file alredy exists")
+
+    else:
+        print("The file does not exist, creating json file..")
+        with open(json_file_path, "w") as file:
+            json.dump([], file)
+            print("The file tasks.json is created sucessfully.")
+
+    # else exists then return the contents (task list) of the file
+    pass
+
+
+# writes to the json file
+def save_tasks():
+    # with open(json_file_path, "w") as file:
+    #     json.dump(["something"], file)
+    pass
+
+
+def main():
+    load_tasks()
+    save_tasks()
+    command = sys.argv[1]
+    task = sys.argv[2:]
+
+    # print(f"command: {command}")
+    # print(f"task: {task}")
+    # print(type(pathlib.Path().absolute()))
+
+    # if command is None:
+    #     print("You did not enter a command")
+    # else:
+    #     if command == "add":
+    #         save_tasks()
+
+
+if __name__ == "__main__":
+    main()
